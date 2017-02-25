a7ffb6010836a1853daee763920a9e6646a9ea85
Brandon High <brandonhigh@gmail.com>
2017-02-25 15:06:00 -0800

    calculating cross entropy

---
+++ intro_to_tensorflow/cross_entropy.py
@@ -0,0 +1,17 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+softmax_data = [0.7, 0.2, 0.1]
+one_hot_data = [1.0, 0.0, 0.0]
+
+softmax = tf.placeholder(tf.float32)
+one_hot = tf.placeholder(tf.float32)
+cross_entropy = -tf.reduce_sum(tf.mul(one_hot, tf.log(softmax)))
+
+# TODO: Print cross entropy from session
+with tf.Session() as session:
+    # Initialize session variables
+    session.run(tf.global_variables_initializer())
+
+    output = session.run(cross_entropy, feed_dict={softmax: softmax_data, one_hot: one_hot_data})
+    print(output)
